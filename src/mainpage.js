d6fdddc2675517f5bdf2267b991d99f67be33e10
SutoBotn <100602639+SutoBotn@users.noreply.github.com>
2022-04-03 17:28:20 +0100

    Changed 'Stuff' button to 'File A Claim' button

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import SubmitClaim from "./submitClaim";
 
 const Mainpage = () =>{
     let navigate = useNavigate();
@@ -12,37 +13,40 @@ const Mainpage = () =>{
         let year = d.getFullYear();
     
         return `${day} ${date} ${month} ${year}`
-      }
+    }
     
-      const onClick = () =>{
+    const onClick = () =>{
         navigate("/")
-      }
+    }
+
+    const fileAClaim = () => {
+        navigate("/submitClaim")
+    }
     return(
-        
         <div className="app">
             <main>
-            <div className="select-use">
-                <select name="use" className="select">
-                <option className="doprdown-content">---select an option---</option>
-                <option className="dropdown-content">File a claim</option>
-                <option className="dropdown-content">View past claims</option>
-                <option className="dropdown-content">View pending claims</option>
-                </select>
-                <button type="button" className="search-button">Search</button>
-                <button type="login" className="login-button" onClick={onClick}>Logout</button>
-            </div>
-            <div className="location-box">
-                <div className="location">London, UK</div>
-                <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="overview">
-                <div className="main-overview">Stuff</div>
-            </div>
+                <div className="select-use">
+                    <select name="use" className="select">
+                    <option className="dropdown-content" disabled selected>---select an option---</option>
+                    <option className="dropdown-content">File a claim</option>
+                    <option className="dropdown-content">View past claims</option>
+                    <option className="dropdown-content">View pending claims</option>
+                    </select>
+                    <button type="button" className="search-button">Search</button>
+                    <button type="login" className="login-button" onClick={onClick}>Logout</button>
+                </div>
+                <div className="location-box">
+                    <div className="location">London, UK</div>
+                    <div className="date">{dateBuilder(new Date())}</div>
+                </div>
+                <div className="overview">
+                    <div className="main-overview" onClick={fileAClaim}>File A Claim</div>
+                </div>
             </main>
             <footer className="footer">
-            <a className="footer-text">FDM Expense Tracker App</a>
+                <a className="footer-text">FDM Expense Tracker App</a>
             </footer>
-      </div>
+        </div>
     );
 }
 export default Mainpage;
