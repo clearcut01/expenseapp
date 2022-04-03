7a4468bd3028e7f95d35cf05a81adb2eafc0662b
SutoBotn <100602639+SutoBotn@users.noreply.github.com>
2022-04-03 17:30:54 +0100

    Added routes for submitting claim

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MainpageLM from "./mainpageLM";
 import MainpageAdmin from "./mainpageAdmin";
 import Login from "./login";
 import {Routes, Route} from "react-router-dom";
+import SubmitClaim from "./submitClaim";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/mainpage" element={<Mainpage />}></Route>
         <Route path="/mainpageAdmin" element={<MainpageAdmin />}></Route>
         <Route path="/mainpageLM" element={<MainpageLM />}></Route>
+        <Route path="/submitClaim" element={<SubmitClaim/>}></Route>
       </Routes>
   );
 }
