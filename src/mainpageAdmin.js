15dcbab99b77b327fbcd99b37e62e9f89b5ab10b
m-stafaq <m.qamar@se20.qmul.ac.uk>
2022-04-13 01:15:13 +0100

    admin page

--- src/mainpageAdmin.js
+++ src/mainpageAdmin.js
@@ -1,4 +1,12 @@
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import SubmitClaim from "./submitClaim";
+import fileclaim from './assets/fileclaim.png';
+import downtime from './assets/downtime.png';
+import reports from './assets/reports.png';
+import manage from './assets/manage.png';
+import notif from './assets/notif.png';
+import settings from './assets/settings.png';
+import register from './assets/register.png';
 
 const Mainpage = () =>{
     let navigate = useNavigate();
@@ -12,37 +20,53 @@ const Mainpage = () =>{
         let year = d.getFullYear();
     
         return `${day} ${date} ${month} ${year}`
-      }
+    }
     
-      const onClick = () =>{
+    const onClick = () =>{
         navigate("/")
-      }
+    }
+
+    const pendingclaim = () => {
+        navigate("/claimApproval")
+    }
     return(
-        
         <div className="app">
             <main>
-            <div className="select-use">
-                <select name="use" className="select">
-                <option className="doprdown-content">---select an option---</option>
-                <option className="dropdown-content">File a claim</option>
-                <option className="dropdown-content">View past claims</option>
-                <option className="dropdown-content">View pending claims</option>
-                </select>
-                <button type="button" className="search-button">Search</button>
-                <button type="login" className="login-button" onClick={onClick}>Logout</button>
-            </div>
-            <div className="location-box">
-                <div className="location">London, UK</div>
-                <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="overview">
-                <div className="main-overview">Admin</div>
-            </div>
+                <div className="select-use">
+                    <p className="title">Administrator Home
+                    <button type="login" className="login-button-LM" onClick={onClick}>Logout</button>
+                    </p>
+                </div>
+                <div className="location-box">
+                    <div className="location">London, UK</div>
+                    <div className="date">{dateBuilder(new Date())}</div>
+                </div>
+                <div className="overview">
+                    <img className="claim" src = {register}></img>
+                    <div className="main-overview">Register Users</div>
+                </div>
+                <div className="overview2">
+                    <img className="currentexpenses" src = {manage}></img>
+                    <div className="current-expenses" >Manage Users</div>
+                </div>
+                <div className="overview3">
+                    <img className="reportsimg" src = {reports}></img>
+                    <div className="reports"> Reports</div>
+                </div>
+                <div className="overview4">
+                    <img className="chimg" src = {downtime}></img>
+                    <div className="claimhistory">Downtime</div>
+                </div>
             </main>
+            
             <footer className="footer">
-            <a className="footer-text">FDM Expense Tracker App</a>
+                <div>
+                    <img className="notif" src = {notif}></img>
+                    <img className="setting" src = {settings}></img>
+                </div>
+                <a className="footer-text">FDM Expense Tracker App</a>
             </footer>
-      </div>
+        </div>
     );
 }
 export default Mainpage;
